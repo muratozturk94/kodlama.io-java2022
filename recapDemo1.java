1b02a6bd9a81a25c4f1d4f91216594074fe9b589
Murat Öztürk <86018413+muratozturk94@users.noreply.github.com>
2022-09-21 05:34:32 +0300

    Add files via upload

---
+++ recapDemo1.java
@@ -0,0 +1,17 @@
+
+public class recapDemo1 {
+
+	public static void main(String[] args) {
+		int s1 = 40;
+		int s2 = 50;
+		int s3 = 30;
+		if(s1>s2 && s1>s3) {
+			System.out.println("En büyük sayı: "+s1);
+		}else if(s2>s1 && s2>s3) {
+			System.out.println("En büyük sayı: "+s2);
+		}else {
+			System.out.println("En büyük sayı: "+s3);
+		}
+	}
+
+}
