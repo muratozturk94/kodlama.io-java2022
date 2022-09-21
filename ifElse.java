59fc3f3a7f3bb0a2a985e69fbd76ffe73b811b60
Murat Öztürk <86018413+muratozturk94@users.noreply.github.com>
2022-09-21 05:15:05 +0300

    Add files via upload

---
+++ ifElse.java
@@ -0,0 +1,15 @@
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		int number = 35;
+		if(number < 30) {
+			System.out.println("Sayı 30'dan küçüktür.");
+		}else if(number == 30) {
+			System.out.println("Sayı 30'a eşittir.");
+		}else {
+			System.out.println("Sayı 30'dan büyüktür.");
+		}
+	}
+
+}
