26013f12b9f2563da3d8c4853970e5f327c1e7ea
Murat Öztürk <86018413+muratozturk94@users.noreply.github.com>
2022-10-05 11:23:20 +0300

    Add files via upload

---
+++ miniProjeSesliHarfler.java
@@ -0,0 +1,37 @@
+
+public class miniProjeSesliHarfler {
+
+	public static void main(String[] args) {
+		char harf = 'I';
+		switch(harf) {
+		case 'A':
+			System.out.println("Kalın sesli harf");
+			break;
+		case 'E':
+			System.out.println("İnce sesli harf");
+			break;
+		case 'I':
+			System.out.println("Kalın sesli harf");
+			break;
+		case 'İ':
+			System.out.println("İnce sesli harf");
+			break;
+		case 'O':
+			System.out.println("Kalın sesli harf");
+			break;
+		case 'Ö':
+			System.out.println("İnce sesli harf");
+			break;
+		case 'U':
+			System.out.println("Kalın sesli harf");
+			break;
+		case 'Ü':
+			System.out.println("İnce sesli harf");
+			break;
+		default:
+			System.out.println("Geçersiz harf girdiniz.");
+			
+		}
+	}
+
+}
