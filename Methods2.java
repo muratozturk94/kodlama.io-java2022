b7bd9f00e8c38bf49a40f4c3732318636e08d193
Murat Öztürk <86018413+muratozturk94@users.noreply.github.com>
2022-10-05 15:37:11 +0300

    Add files via upload

--- Methods2.java
+++ Methods2.java
@@ -7,6 +7,8 @@ public class Methods2 {
 		System.out.println(yeniMesaj);
 		int sayi = topla(5,7);
 		System.out.println(sayi);
+		int toplam = topla2(2,3,4,5,6,10);
+		System.out.println(toplam);
 	}
 	
 	
@@ -24,6 +26,14 @@ public class Methods2 {
 	
 	public static int topla(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
+	}
+	
+	public static int topla2(int... sayilar) {
+		int toplam=0;
+		for(int sayi:sayilar) {
+			toplam+=sayi;
+		}
+		return toplam;
 	}
 	
 	public static String sehirVer() {
